90470840bf8a65de61ccc9db3c95643ca91ff076
Unicar <weijia@baseup.com.au>
2018-04-16 16:24:34 +1000

    Added selectionReducer

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Text, TouchableWithoutFeedback } from 'react-native'
+import { connect } from 'react-redux'
 import { CardSection } from './common'
 import * as actions from '../actions'
+
 
 class ListItem extends Component {
     render() {
         const { titleStyle } = styles
+        const { id, title } = this.props.library
+        console.log(this.props)
         return (
-            <CardSection>
-                <Text style={titleStyle}>{this.props.library.title}</Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>{title}</Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
         )
     }
 }
@@ -21,3 +31,4 @@ const styles = {
     }
 }
 
+export default connect(null, actions)(ListItem)
