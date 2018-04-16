c511127894b02bba2d7af56e2b40cf28b73dd50c
Unicar <unicar9@gmail.com>
2018-04-17 08:04:08 +1000

    Added firebase to manager

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react'
 import { View, Text } from "react-native"
 import { Provider } from "react-redux"
 import { createStore } from "redux"
-import reducers from "./reducers";
+import reducers from "./reducers"
+import firebase from 'firebase'
+
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyDVxr6vu9Q7OBRt4LLow6WNcWevT-Z1PFs",
+            authDomain: "manager-c8d6f.firebaseapp.com",
+            databaseURL: "https://manager-c8d6f.firebaseio.com",
+            projectId: "manager-c8d6f",
+            storageBucket: "manager-c8d6f.appspot.com",
+            messagingSenderId: "295968628423"
+        }
+        firebase.initializeApp(config)
+    }
     render() {
         return (
             <Provider store={createStore(reducers)}>
